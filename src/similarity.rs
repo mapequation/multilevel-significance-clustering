10f014d97e549d2ff9c056c0a9e354f2b04c3138
Anton Eriksson <anton.eriksson@umu.se>
2022-03-13 21:41:46 +0100

    Refactor similarity

--- src/similarity.rs
+++ src/similarity.rs
@@ -7,34 +7,32 @@ pub fn get_most_similar_modules(
     first: &Network,
     rest: &BTreeMap<usize, Network>,
 ) -> HashMap<String, BTreeMap<usize, String>> {
-    let mut most_similar_modules = HashMap::new();
-
-    for module1 in first.modules.values() {
-        let most_similar_to_module = most_similar_modules
-            .entry(module1.module_id.clone())
-            .or_insert_with(BTreeMap::new);
-
-        rest.iter().for_each(|(network_id, network)| {
-            let mut distances = network
-                .modules
-                .values()
-                .map(|module2| {
-                    (
-                        &module2.module_id,
-                        jaccard_distance(&module1.nodes, &module2.nodes),
-                    )
+    first
+        .modules
+        .values()
+        .map(|module1| {
+            let most_similar = rest
+                .iter()
+                .filter_map(|(&network_id, network)| {
+                    network
+                        .modules
+                        .values()
+                        .map(|module2| {
+                            (
+                                &module2.module_id,
+                                jaccard_distance(&module1.nodes, &module2.nodes),
+                            )
+                        })
+                        .min_by(|(_, distance1), (_, distance2)| {
+                            distance1.partial_cmp(distance2).unwrap()
+                        })
+                        .and_then(|(module_id, ..)| Some((network_id, module_id.clone())))
                 })
-                .collect::<Vec<_>>();
+                .collect();
 
-            distances.sort_unstable_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
-
-            if let Some(distance) = distances.first() {
-                most_similar_to_module.insert(*network_id, distance.0.clone());
-            }
-        });
-    }
-
-    most_similar_modules
+            (module1.module_id.clone(), most_similar)
+        })
+        .collect()
 }
 
 fn jaccard_distance(m1: &HashSet<NodeId>, m2: &HashSet<NodeId>) -> f32 {
